1f401cd2d86ad35ffbacc931a1e3218d544f9fd7
FacuBisio <facubisio410@gmail.com>
2026-05-19 13:13:03 -0300

    Base practico IV
    Base inicial

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="assets/css/styles.css">
+</head>
+<body>
+    <section id="navbar">
+        
+        <div class="logo">
+            <h1>HDSGADKSJ</h1>
+        </div>
+
+        <div class="nav-links">
+            <a href="#">dsadss</a>
+            <a href="#">dsadss</a>
+            <a href="#">dsadss</a>
+        </div>
+
+    </section>
+    
+</body>
+</html>
