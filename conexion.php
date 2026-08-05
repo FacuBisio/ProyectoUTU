6d66e84b0a8da9fa5fbadd251051a4cfa4f76886
sr5006838-debug <sr5006838@gmail.com>
2026-08-04 21:41:25 -0300

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -2,7 +2,7 @@
 $servidor = "localhost";
 $usuario = "root";
 $password = "";
-$baseDatos = "ProyectoUtu";
+$baseDatos = "proyectoutu";
 $conexion = new mysqli($servidor, $usuario, $password, $baseDatos);
 if($conexion->connect_error){
 die("Error de conexión: " . $conexion->connect_error);
